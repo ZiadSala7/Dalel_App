4670e7b2e27f591c49ac1595f9b98c07c6169287
Ziad Salah <zslah2800@gmail.com>
2024-08-07 19:33:33 +0300

    app styles created

---
+++ lib/core/utils/app_styles.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AppStyles {
+  static const TextStyle textStyle25 = TextStyle(
+    fontSize: 25,
+  );
+
+  static const TextStyle textStyle22 = TextStyle(
+    fontSize: 22,
+  );
+
+  static const TextStyle textStyle30 = TextStyle(
+    fontSize: 30,
+  );
+
+  static const TextStyle textStyle18 = TextStyle(
+    fontSize: 18,
+  );
+}
