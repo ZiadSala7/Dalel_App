da7f42892ad933aea87be2f6fa67b9664bb4bc28
Ziad Salah <zslah2800@gmail.com>
2024-08-07 18:49:16 +0300

    adding splash view to go router

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,7 +1,13 @@
+import 'package:dalel_app/features/splash/presentation/views/splash_view.dart';
 import 'package:go_router/go_router.dart';
 
 class AppRouter {
-  static final router = GoRouter(routes: [
-    // GoRoute(path: '/', builder: (context, state) => SplashView(),)
-  ]);
+  static final router = GoRouter(
+    routes: [
+      GoRoute(
+        path: '/',
+        builder: (context, state) => const SplashView(),
+      ),
+    ],
+  );
 }
