1a2bfaa46247f5ed4dc9407c303fd86ad3ce75c3
Ziad Salah <zslah2800@gmail.com>
2024-08-08 12:30:08 +0300

    adding some images to app images

--- lib/core/utils/app_images.dart
+++ lib/core/utils/app_images.dart
@@ -1,6 +1,10 @@
 class AppImages {
   static const String splashImg = "assets/imgs/Dalel.png";
+  static const String dalel = "assets/imgs/Dalel2.png";
   static const String onboarding1 = "assets/imgs/onboarding1.png";
   static const String onboarding2 = "assets/imgs/onboarding2.png";
   static const String onboarding3 = "assets/imgs/onboarding3.png";
+  static const String login = "assets/imgs/login.png";
+  static const String login2 = "assets/imgs/login2.png";
+  static const String login3 = "assets/imgs/login3.png";
 }
