69e1c272b9ef13d7b04436beda7c781ac0ebb162
Ziad Salah <zslah2800@gmail.com>
2024-08-08 20:28:31 +0300

    adding login view to app router

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,3 +1,4 @@
+import 'package:dalel_app/features/login/presentation/views/login_view.dart';
 import 'package:dalel_app/features/onboarding/presentation/views/onboarding_view.dart';
 import 'package:dalel_app/features/register/presentation/views/register_view.dart';
 import 'package:dalel_app/features/splash/presentation/views/splash_view.dart';
@@ -6,6 +7,7 @@ import 'package:go_router/go_router.dart';
 class AppRouter {
   static const String onBoarding = '/onBoarding';
   static const String registerView = '/registerView';
+  static const String loginView = '/loginView';
   static final router = GoRouter(
     routes: [
       GoRoute(
@@ -19,6 +21,10 @@ class AppRouter {
       GoRoute(
         path: AppRouter.registerView,
         builder: (context, state) => const RegisterView(),
+      ),
+      GoRoute(
+        path: AppRouter.loginView,
+        builder: (context, state) => const LoginView(),
       ),
     ],
   );
