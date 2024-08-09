2065d890c4bf745cd78da2282adf049d8c09360e
Ziad Salah <zslah2800@gmail.com>
2024-08-09 15:22:07 +0300

    half of verification view

--- lib/features/forget_password/presentation/views/verify_password_view.dart
+++ lib/features/forget_password/presentation/views/verify_password_view.dart
@@ -1,3 +1,5 @@
+import 'package:dalel_app/constants.dart';
+import 'package:dalel_app/core/utils/app_styles.dart';
 import 'package:flutter/material.dart';
 
 class VerifyPasswordView extends StatelessWidget {
@@ -5,6 +7,44 @@ class VerifyPasswordView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body: VerifyPasswordViewBody(),
+    );
+  }
+}
+
+class VerifyPasswordViewBody extends StatelessWidget {
+  const VerifyPasswordViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          const SizedBox(
+            height: 100,
+          ),
+          Text(
+            'Verify Account',
+            textAlign: TextAlign.center,
+            style: AppStyles.textStyle30.copyWith(
+              fontFamily: dalelBold,
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Text(
+            'Enter 4 digit code we have sent \nto testing@gmail.com',
+            textAlign: TextAlign.center,
+            style: AppStyles.textStyle18.copyWith(
+              fontFamily: dalelReg,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
