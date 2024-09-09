9daea0d4072154f7c7f25712ff8e23fda283fb3e
ZiadSala7 <zslah2800@gmail.com>
2024-09-09 11:23:03 +0300

    adding firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:dalel_app/core/utils/app_router.dart';
+import 'package:dalel_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
