856f901faf1200a9283e8172f0cd5c02918cbfe7
ZiadSala7 <zslah2800@gmail.com>
2024-09-17 14:01:44 +0300

    f

--- lib/features/home/presentation/views/widgets/historical_period_section.dart
+++ lib/features/home/presentation/views/widgets/historical_period_section.dart
@@ -1,6 +1,7 @@
 import 'package:dalel_app/core/utils/app_images.dart';
 import 'package:dalel_app/features/home/presentation/views/widgets/historical_period_item.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class HistoricalPeriodSection extends StatelessWidget {
   const HistoricalPeriodSection({
@@ -9,18 +10,23 @@ class HistoricalPeriodSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        HistoricalPeriodItem(
-          text: 'Ancient\nEgypt',
-          image: AppImages.ancientEgypt,
-        ),
-        HistoricalPeriodItem(
-          text: 'Islamic\nEra',
-          image: AppImages.islamicEra,
-        ),
-      ],
+    return FutureBuilder<QuerySnapshot>(
+      future: null,
+      builder: (context, snapShot) {
+        return const Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            HistoricalPeriodItem(
+              text: 'Ancient\nEgypt',
+              image: AppImages.ancientEgypt,
+            ),
+            HistoricalPeriodItem(
+              text: 'Islamic\nEra',
+              image: AppImages.islamicEra,
+            ),
+          ],
+        );
+      },
     );
   }
 }
